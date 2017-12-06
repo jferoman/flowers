37f76a86a1ef70550051c761251c09046b36cbe3
Duvan Castelblanco <duvancastelblanco@hotmail.com>
2017-12-06 09:13:09 -0500

    add index uniq test

--- app/models/bed.rb
+++ app/models/bed.rb
@@ -1,7 +1,6 @@
 class Bed < ApplicationRecord
 
-  validates_uniqueness_of :number
-  validates_presence_of   :number, :total_area, :usable_area, :block_id
+  validates_presence_of :number, :total_area, :usable_area, :block_id
 
   validates_numericality_of :total_area, :allow_nil => false, :greater_than => 0.0
   validates_numericality_of :usable_area, :allow_nil => false, :greater_than => 0.0
--- spec/models/bed_spec.rb
+++ spec/models/bed_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Bed do
   describe '#create' do
+
     it { should validate_presence_of(:number) }
     it { should validate_presence_of(:total_area) }
     it { should validate_presence_of(:usable_area) }
