66f4ff70384641c9ce58a3da97054842ed92eae7
Duvan Castelblanco <duvancastelblanco@hotmail.com>
2017-12-06 09:16:13 -0500

    add presense bed  test

--- app/models/bed.rb
+++ app/models/bed.rb
@@ -1,6 +1,6 @@
 class Bed < ApplicationRecord
 
-  validates_presence_of :number, :total_area, :usable_area, :block_id
+  validates_presence_of :number, :total_area, :usable_area, :block_id, :bed_type_id
 
   validates_numericality_of :total_area, :allow_nil => false, :greater_than => 0.0
   validates_numericality_of :usable_area, :allow_nil => false, :greater_than => 0.0
--- spec/models/bed_spec.rb
+++ spec/models/bed_spec.rb
@@ -7,6 +7,7 @@ describe Bed do
     it { should validate_presence_of(:total_area) }
     it { should validate_presence_of(:usable_area) }
     it { should validate_presence_of(:block_id) }
+    it { should validate_presence_of(:bed_type_id) }
 
     it { should validate_numericality_of(:total_area).is_greater_than(0.0) }
     it { should validate_numericality_of(:usable_area).is_greater_than(0.0) }
