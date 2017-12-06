f99b12014163dd05270e5921f9014a3ffdea3634
Duvan Castelblanco <duvancastelblanco@hotmail.com>
2017-12-06 10:30:03 -0500

    tests

--- spec/models/block_spec.rb
+++ spec/models/block_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 describe Block do
   describe '#create' do
-    #it { should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:farm_id) }
+    it { should have_db_index( [:name, :farm_id] ).unique(true) }
+
   end
 
   describe 'associations' do
     it { should belong_to(:farm) }
     it { should have_many(:beds) }
+    it { should have_many(:block_color_flowers) }
   end
 end
--- spec/models/coldroom_spec.rb
+++ spec/models/coldroom_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Coldroom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Coldroom do
+  describe '#create' do
+
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:capacity) }
+    it { should validate_presence_of(:farm_id) }
+
+    it { should validate_numericality_of(:capacity).is_greater_than(0) }
+
+  end
+
+  describe 'associations' do
+    it { should belong_to(:farm) }
+  end
 end
