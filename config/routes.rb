2f979e1086ab2ad3c78f4e9215eb07c89cd5051c
Duvan Castelblanco <duvancastelblanco@hotmail.com>
2018-01-10 12:44:01 -0500

    Borrado de calendario

--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,7 @@ Rails.application.routes.draw do
   delete '/beds' => 'beds#batch_delete', as: :beds_batch_delete
   delete '/demands' => 'demands#batch_delete', as: :demands_batch_delete
   delete '/color_submarkets' => 'color_submarkets#batch_delete', as: :color_submarkets_batch_delete
+  delete '/weeks' => 'weeks#batch_delete', as: :weeks_batch_delete
 
 
 
